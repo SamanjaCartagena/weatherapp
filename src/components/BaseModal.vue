749d8e5ebbc6ba36b1d93c33a80e356749f348be
Samanja Cartagena <c.samanja09@Pabs.local>
2024-08-01 02:47:34 -0400

    search results added

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -1,9 +1,11 @@
 <template>
+<teleport to="body">
+<transition name="modal-outer">
 <div 
  v-show="modalActive"
  class='absolute w-full bg-black 
  bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8'>
-
+<transition name="modal-inner">
 <div 
  v-if="modalActive"
  class="p-4 bg-white self-start mt-32 max-w-screen-md">
@@ -12,8 +14,10 @@
      @click="$emit('close-modal')"
      class="text-white mt-8 bg-black py-2 px-6">Close</button>
 </div>
+</transition>
 </div>
-  
+</transition>
+</teleport>
 </template>
 
 <script setup>
@@ -28,6 +32,21 @@ defineProps({
 
 </script>
 
-<style>
+<style scoped>
+.modal-outer-enter-active,
+.modal-outer-leave-active{
+    transition: opacity 0.3s cubic-bezier(0.52,0.02,0.19,1.02);
+}
 
+.modal-outer-enter-from,
+.modal-outer-leave-to{
+    opacity:0;
+}
+.modal-inner-enter-from{
+    opacity:0;
+    transform:scale(0.8)
+}
+.modal-inner-leave-to{
+    transform:scale(0.8)
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,50 @@
-<script setup>
-</script>
-
 <template>
-  <main>
+  <main class="container text-black">
+    <div class='pt-4 mb-8 relative'>
+      <input 
+      v-model="searchQuery"
+      @input="getSearchResults"
+      type="text" placeholder="Search for a city or a "
+      class="py-2 px-1 w-full bg-transparent border-b focus:border-weather-seconndary focus:outline-none
+      focus:shadow-[0px_1px_0_0)#004E71]
+      "/>
+      <ul
+      v-if="mapboxSearchResults"
+      class='absolute bg-weather-secondary text-black w-full shadow-md py-2 px-1 top-[66px]'>
+         <li v-for='searchResult in mapboxSearchResults' :key="searchResult.id"
+         class="py-2 cursor-pointer"
+         >
+           {{searchResult.place_name}}
+         </li>
+
+      </ul>
+    </div>
   </main>
 </template>
+
+<script setup>
+import {ref} from 'vue'
+import axios from 'axios'
+
+const mapboxSearchResults=ref(null)
+const mapoboxAPIKey="pk.eyJ1Ijoic2FtYW5qYTEyMyIsImEiOiJjbHphc2JpeHowcmpzMmlvZDB4MWl1aW9sIn0.8qSiGV_qeMZnorHFoej9JA"
+const searchQuery = ref('')
+const queryTimeout= ref(null)
+const getSearchResults= () => {
+  clearTimeout(queryTimeout.value)
+  queryTimeout.value=setTimeout(async()=> {
+   if (searchQuery.value !== ''){
+    const result =await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapoboxAPIKey}&types=place`)
+    mapboxSearchResults.value = result.data.features;
+    console.log(mapboxSearchResults.value)
+    return;
+   }
+   mapboxSearchResults.value= null
+  },300)
+}
+
+</script>
+
+<style>
+
+</style>
